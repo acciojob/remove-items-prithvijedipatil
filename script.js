ef65fc53b84edb4ca2c3125773b9fc046a9dc57e
prithvijedipatil <prithvipatil09@gmail.com>
2023-06-19 12:52:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded",funtion(){
+
+	const removebut = document.getElementById("rem");
+	const sel = document.getElementById("colorSelect");
+
+	removebut.addEventListener("click",funtion(){
+
+		const selcol = sel.options[sel.selectedIndex];
+		sel.removeChild(selcol);
+		
+	});
+});
